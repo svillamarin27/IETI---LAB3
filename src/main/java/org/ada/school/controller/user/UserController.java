c0134319900ee13dc1b81067a31fa0753fd98338
Sebastián Villamarín Rodríguez <sebastian.villamarin@mail.escuelaing.edu.co>
2021-09-03 12:20:02 -0500

    part 3.5

--- src/main/java/org/ada/school/controller/user/UserController.java
+++ src/main/java/org/ada/school/controller/user/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -42,6 +43,7 @@ public class UserController
 
 
     @PostMapping
+    @RolesAllowed("ADMIN")
     public ResponseEntity<User> create( @RequestBody UserDto userDto )
     {
         return ResponseEntity.ok( userService.create( userDto ) );
